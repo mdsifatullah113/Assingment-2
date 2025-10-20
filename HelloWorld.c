87db7320724b886db260a660d34a7b280784e885
sifatullahsifat@gmail.com <sifatullahsifat678@gmail.com>
2025-10-20 08:32:36 +0600

    First commit:print Hello world 4 times

---
+++ HelloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int numbers = 3;
+    for (int i = 1; i <= numbers; i++)
+    {
+        printf("Hello World\n");
+    }
+    printf("Hello Bangladesh\n");
+    return 0;
+}
